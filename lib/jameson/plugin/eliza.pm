685b5dd853f7320cb78cb90ad17361a59f460e5d
Robert Norris <rob@eatenbyagrue.org>
2012-08-05 21:55:39 +0200

    first cut. connects and distributes messages to plugins

---
+++ lib/jameson/plugin/eliza.pm
@@ -0,0 +1,21 @@
+package jameson::plugin::eliza;
+
+use 5.016;
+use warnings;
+use strict;
+
+use base 'jameson::plugin';
+
+use Chatbot::Eliza;
+
+my $eliza = Chatbot::Eliza->new;
+
+sub publicmsg {
+    my ($self, $con, $channel, $from, $text, $direct) = @_;
+    return if not $direct;
+
+    my $out = $eliza->transform($text);
+    $con->send_srv(PRIVMSG => $channel, "$from: $out");
+}
+
+1;
