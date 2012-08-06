32fa97137be0b94e8e1fd8867e61ebdbf862d18b
Robert Norris <rob@eatenbyagrue.org>
2012-08-06 19:58:04 +0200

    make url shortening a utility function

--- lib/jameson/plugin/github.pm
+++ lib/jameson/plugin/github.pm
@@ -12,8 +12,6 @@ use JSON;
 
 my $ISSUE_BASE = "https://api.github.com/repos/pioneerspacesim/pioneer/issues";
 
-my %shorturl_cache;
-
 sub publicmsg {
     my ($self, $con, $channel, $from, $text, $direct) = @_;
 
@@ -25,34 +23,15 @@ sub publicmsg {
 
             my $data = decode_json($body);
 
-            my $post = sub {
+            jameson::util::shorten($data->{html_url}, sub {
                 my ($shorturl) = @_;
 
                 my $type = $data->{pull_request}->{diff_url} ? "PR" : "Issue";
 
-                my $msg = "$type #$issue: $data->{title}";
-                $msg .= " $shorturl" if $shorturl;
+                my $msg = "$type #$issue: $data->{title} $shorturl";
 
                 $con->send_srv(PRIVMSG => $channel, $msg);
-            };
-
-            my $url = $data->{html_url};
-
-            if (exists $shorturl_cache{$url}) {
-                $post->($shorturl_cache{$url});
-            }
-            else {
-                http_get("http://is.gd/create.php?url=$data->{html_url}&format=simple", sub {
-                    my ($body, $hdr) = @_;
-                    if ($hdr->{Status} =~ m/^2/) {
-                        $shorturl_cache{$url} = $body;
-                        $post->($body);
-                    }
-                    else {
-                        $post->();
-                    }
-                });
-            }
+            });
         });
     }
 }
