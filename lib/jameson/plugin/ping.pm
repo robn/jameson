6c281ad6769005bf0582eec2e941f8ec0a541604
Robert Norris <rob@eatenbyagrue.org>
2012-08-17 23:12:20 +0200

    ping module for synchronous "are you there" test

---
+++ lib/jameson/plugin/ping.pm
@@ -0,0 +1,18 @@
+package jameson::plugin::ping;
+
+use 5.016;
+use warnings;
+use strict;
+
+use base 'jameson::plugin';
+
+sub publicmsg {
+    my ($self, $con, $channel, $from, $text, $direct) = @_;
+    return if not $direct;
+
+    return unless $text =~ m/^\s*ping\s*$/;
+
+    $con->send_srv(PRIVMSG => $channel, "$from: pong");
+}
+
+1;
