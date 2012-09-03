b8d1b58e14223400a45b63d1960edb9430cc1f3d
Robert Norris <rob@eatenbyagrue.org>
2012-09-03 17:54:11 +1000

    karma plugin

---
+++ lib/jameson/plugin/karma.pm
@@ -0,0 +1,38 @@
+package jameson::plugin::karma;
+
+use 5.016;
+use warnings;
+use strict;
+
+use base 'jameson::plugin';
+
+use Storable;
+use Lingua::EN::Inflect qw(inflect);
+
+my %karma;
+my $karmafile = "karma.db";
+
+sub init {
+	%karma = eval { %{retrieve $karmafile} };
+}
+
+sub publicmsg {
+    my ($self, $con, $channel, $from, $text, $direct) = @_;
+
+    if ($direct) {
+        my ($who) = $text =~ m/karma\s+(\w+)/i;
+        return unless $who;
+
+        my $karma = $karma{lc $who} // 0;
+
+        $con->send_srv(PRIVMSG => $channel, inflect "$who has NUM($karma) PL_N(point)");
+        return;
+    }
+
+    while (my ($who, $updown) = $text =~ m/\G.*?(\w+)\s*(\+\+|--)/gc) {
+        $karma{$who} += $updown eq '++' ? 1 : -1;
+		store \%karma, $karmafile;
+    }
+}
+
+1;
